a324028a70ec01be9654fb51ff67df623d3f31c8
Max Van Ling <maxvanling@Maxs-MacBook-Pro.local>
2018-01-04 20:20:34 +0100

    updated setup to make it work

--- setup.sh
+++ setup.sh
@@ -4,6 +4,10 @@ echo "please enter the coin you mine (full name without capital letters):"
 read COIN
 echo "please enter your addres you mine to:"
 read ADDRESS
+touch config.json
 echo "{\"address\": \"$ADDRESS\", \"crypto\": \"$COIN\"}" >> config.json
+rm data.json
+touch data.json
+echo "{\"hashrate\": [0], \"price\": [0], \"balance\": [0], \"online\": false}"
 echo "isntalling cfscrape"
 pip install cfscrape
