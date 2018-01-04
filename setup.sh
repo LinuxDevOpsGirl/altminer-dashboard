e236e90d7095dabe07cba47dfcedc44a240c6802
Max Van Ling <maxvanling@Maxs-MacBook-Pro.local>
2018-01-04 20:26:15 +0100

    ok. now it works

--- setup.sh
+++ setup.sh
@@ -8,6 +8,6 @@ touch config.json
 echo "{\"address\": \"$ADDRESS\", \"crypto\": \"$COIN\"}" >> config.json
 rm data.json
 touch data.json
-echo "{\"hashrate\": [0], \"price\": [0], \"balance\": [0], \"online\": false}"
+echo "{\"hashrate\": [0], \"price\": [0], \"balance\": [0], \"online\": false}" >> data.json
 echo "isntalling cfscrape"
 pip install cfscrape
