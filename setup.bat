ee73deefd752ad986e348c5c50fb94d2a695e003
maxer137 <maxer137@gmail.com>
2018-01-05 20:33:26 +0100

    added windows support

---
+++ setup.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+del config.json
+SET /P COIN=please enter the coin you mine (full name without capital letters):
+SET /P ADDRESS=please enter your addres you mine to:
+copy /y nul config.json
+echo {"address": "%ADDRESS%", "crypto": "%COIN%"} >> config.json
+del data.json
+copy /y nul data.json
+echo {"hashrate": [1], "price": [1], "balance": [1], "online": false} >> data.json
+echo isntalling cfscrape
+pip install cfscrape
+echo installing colorama
+pip install colorama
+@ECHO ON
